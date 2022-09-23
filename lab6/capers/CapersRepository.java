81e4dce7f9737022d329900c06288fe0e461d341
Sol1tudeRain <silverarc600@gmail.com>
2022-09-23 10:36:39 +0800

    lab6 done

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -4,22 +4,22 @@ import java.io.File;
 import static capers.Utils.*;
 
 /** A repository for Capers 
- * @author TODO
+ * @author Sol1tudeRain
  * The structure of a Capers Repository is as follows:
  *
  * .capers/ -- top level folder for all persistent data in your lab12 folder
  *    - dogs/ -- folder containing all of the persistent data for dogs
  *    - story -- file containing the current story
  *
- * TODO: change the above structure if you do something different.
+ *
  */
 public class CapersRepository {
     /** Current Working Directory. */
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    static final File CAPERS_FOLDER = join(CWD,".capers");
+
 
     /**
      * Does required filesystem operations to allow for persistence.
@@ -31,7 +31,8 @@ public class CapersRepository {
      *    - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        CAPERS_FOLDER.mkdir();
+        Dog.DOG_FOLDER.mkdir();
     }
 
     /**
@@ -40,7 +41,16 @@ public class CapersRepository {
      * @param text String of the text to be appended to the story
      */
     public static void writeStory(String text) {
-        // TODO
+        File story = join(CAPERS_FOLDER, "story");
+        String content_to_be_written;
+        if(!story.exists()){
+            content_to_be_written=text;
+        }else{
+            String storyContent = readContentsAsString(story);
+            content_to_be_written = storyContent + "\n" + text;
+        }
+        writeContents(story, content_to_be_written);
+        System.out.println(content_to_be_written);
     }
 
     /**
@@ -49,7 +59,9 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog newDog = new Dog(name, breed, age);
+        newDog.saveDog();
+        System.out.println(newDog);
     }
 
     /**
@@ -59,6 +71,8 @@ public class CapersRepository {
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+        Dog dogFromFile = Dog.fromFile(name);
+        dogFromFile.haveBirthday();
+        dogFromFile.saveDog();
     }
 }
--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -67,9 +67,6 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
         size++;
     }
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
 
     public int size() {
         return size;
