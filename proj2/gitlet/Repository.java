df4117012fcec39affc57bf2fadda56e2ba8dffe
Sol1tudeRain <silverarc600@gmail.com>
2022-10-01 13:14:34 +0800

    fix short UID problem

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -162,7 +162,7 @@ public class Repository {
         }
 
         newCommit.UID=sha1(serialize(newCommit));
-        gitletState.shortID.put(newCommit.UID.substring(0,4), newCommit.UID);
+        gitletState.shortID.put(newCommit.UID.substring(0,8), newCommit.UID);
         newCommit.save();
 
         clearDir(STAGING_DIR);
